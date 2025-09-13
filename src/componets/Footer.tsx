e6f41cf19a0f59521cda0e96ce2634cb23776171
Guilherme <guittrex.2005@gmail.com>
2025-09-13 12:39:24 -0300

    Página básica com Header e Footer adicionada, criado a estilização de reset e de responsividade, que será usada depois.

---
+++ src/componets/Footer.tsx
@@ -0,0 +1,9 @@
+// Componente do rodapé da página
+export default function Footer() {
+    return(
+        <footer>
+            <p>Feito por Guilherme Mendonça</p>
+            <p>Usando Next.js e PokeAPI</p>
+        </footer>
+    )
+}
---
+++ src/componets/Header.tsx
@@ -0,0 +1,8 @@
+// Componente do cabeçalho da página
+export default function Header() {
+    return (
+        <header>
+            <h1>Pokedex</h1>
+        </header>
+    )
+}
